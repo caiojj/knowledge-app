e12fd34675a9623a028ce048d271a5e71cfe842b
caiojj <leal22caio@gmail.com>
2021-08-22 19:32:55 -0300

    Splash screen

--- app/src/main/java/br/com/knowledge/ui/SplashScreenActivity.kt
+++ app/src/main/java/br/com/knowledge/ui/SplashScreenActivity.kt
@@ -1,12 +1,23 @@
 package br.com.knowledge.ui
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import br.com.knowledge.R
+import android.os.Handler
+import android.os.Looper
+import br.com.knowledge.databinding.ActivitySplashScreenBinding
 
 class SplashScreenActivity : AppCompatActivity() {
+
+    private val binding by lazy { ActivitySplashScreenBinding.inflate(layoutInflater) }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash_screen)
+        setContentView(binding.root)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 2000)
     }
 }
