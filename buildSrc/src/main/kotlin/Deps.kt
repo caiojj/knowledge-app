2f7c846554d56becb7b131f775688b780517a685
caiojj <leal22caio@gmail.com>
2021-09-02 22:16:06 -0300

    Object Deps

---
+++ buildSrc/src/main/kotlin/Deps.kt
@@ -0,0 +1,4 @@
+object Deps {
+    const val gradlePlugin = "org.jetbrains.kotlin:kotlin-gradle-plugin:${Versions.KOTLIN}"
+    const val buildGradle = "com.android.tools.build:gradle:${Versions.GRADLE}"
+}
